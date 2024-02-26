ebf06952a1d62f917552d614db5a2eacde17beb9
fazil <fazil424@gmail.com>
2024-02-26 23:09:34 +0530

    LinkedList first commit

---
+++ DSA1/src/dataStructures/llinkedList/LinkedList.java
@@ -0,0 +1,36 @@
+package dataStructures.llinkedList;
+
+public class LinkedList {
+
+	private Node head;
+	private Node tail;
+	private long length;
+
+	class Node {
+		int value;
+		Node next;
+
+		Node(int value) {
+			this.value = value;
+
+		}
+	}
+
+	public LinkedList(int value) {
+		Node newNode = new Node(value);
+		head = newNode;
+		tail = newNode;
+		length = 1;
+
+	}
+	
+	
+	public void print() {
+		Node temp= head;
+		while(temp!=null) {
+			System.out.println(temp.value);
+			temp= temp.next;
+		}
+	}
+
+}
---
+++ DSA1/src/dataStructures/llinkedList/Main.java
@@ -0,0 +1,13 @@
+package dataStructures.llinkedList;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	LinkedList ll = new LinkedList(23);
+	ll.print();
+	
+	
+	}
+
+}
