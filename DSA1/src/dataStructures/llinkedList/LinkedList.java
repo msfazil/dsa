33e0b5ea89a2b391ef56af69c3d39664b566b995
fazil <fazil424@gmail.com>
2024-02-27 23:20:15 +0530

    print and append methods

--- DSA1/src/dataStructures/llinkedList/LinkedList.java
+++ DSA1/src/dataStructures/llinkedList/LinkedList.java
@@ -23,14 +23,38 @@ public class LinkedList {
 		length = 1;
 
 	}
-	
-	
+
 	public void print() {
-		Node temp= head;
-		while(temp!=null) {
+		Node temp = head;
+		while (temp != null) {
 			System.out.println(temp.value);
-			temp= temp.next;
+			temp = temp.next;
 		}
+	}
+
+	public void getHead() {
+		System.out.println("Head " + head);
+	}
+
+	public void getTail() {
+		System.out.println("Tail " + tail);
+	}
+
+	public void getLength() {
+		System.out.println("Lenght " + length);
+	}
+
+	public void append(int value) {
+		Node newNode = new Node(value);
+		if (length == 0) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+		length++;
+
 	}
 
 }
--- DSA1/src/dataStructures/llinkedList/Main.java
+++ DSA1/src/dataStructures/llinkedList/Main.java
@@ -5,8 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 
 	LinkedList ll = new LinkedList(23);
+	
+	
+	
+	ll.append(2);
+	ll.append(27);
 	ll.print();
 	
+	ll.getLength();
 	
 	}
 
